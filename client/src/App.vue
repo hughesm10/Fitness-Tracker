b347225e7832b18dde5c6e6e0f3a6d085a5582fc
hughesm10 <hughesm10@newpaltz.edu>
2021-10-18 12:54:15 -0400

    updates

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <Nav />
+
+                <router-view/>                 
+            </div>
+        </div>
+       
+    </div>
+
+</template>
+
+<style lang="scss">
+</style>
+
+<script>
+import Nav from "./components/Nav";
+export default {
+    components: {
+        Nav
+    }
+}
+</script>
