91e4ff11e65382e9501575ff6ff2beb666f9819f
hughesm10 <hughesm10@newpaltz.edu>
2021-10-27 18:07:38 -0400

    new, organized folders

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,26 @@
 <template>
-    <div class="container">
-        <div class="columns">
-            <div class="column">
-                <Nav />
-
-                <router-view/>                 
-            </div>
-        </div>
-       
-    </div>
-
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<style lang="scss">
-</style>
-
 <script>
-import Nav from "./components/Nav";
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-    components: {
-        Nav
-    }
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
