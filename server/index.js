c675aa8d88274d7ce91b8b3dec01a3ea0d2dd40c
hughesm10 <hughesm10@newpaltz.edu>
2021-12-01 00:23:16 -0500

    path fix

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const path = require('path');
+
 const app = express()
 const port = process.env.PORT || 3000;
 
+app
+    .use('/', express.static(path.join(__dirname, '../docs')) )
+
+    /*
+        Access-Control-Allow-Origin: https://foo.example
+        Access-Control-Allow-Methods: POST, GET, OPTIONS
+        Access-Control-Allow-Headers: X-PINGOTHER, Content-Type
+    */
+
+    .use( (req, res, next) =>{
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', '*');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        next();
+    } )
+
+    .use(express.json())
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../docs/index.php'))
-})
+    res.sendFile(path.join(__dirname, '../docs/index.php'))
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
